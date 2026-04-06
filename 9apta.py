44fc8af7e3e6f69ff21ed418bfe1f38c94868501
Сандугаш Жеткерген <sandugaszetkergen@MacBook-Air-Sandugas.local>
2026-04-06 16:54:34 +0500

    first commit

---
+++ 9apta.py
@@ -0,0 +1,20 @@
+# import numpy as np
+import pandas as pd
+class ReportAnalyzer:
+    def __init__(self,path):
+        self.path = path
+        self.data = None
+    def load(self):
+        self.data = pd.read_csv(self.path)
+    def calc_mean(self):
+        cols = self.data.select_dtypes(include="number").columns[:3]
+        means={}
+        for c in cols:
+            means[c]=round(self.data[c].mean(),2)
+        return means
+if __name__=="__main__":
+    print("Zhetkergen Sandugash")
+    ra = ReportAnalyzer("report.csv")
+    ra.load()
+    result=ra.calc_mean()
+    print(result)

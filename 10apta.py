a3b77d897239b8b72c1e42a43e84449175041350
sandugashzhetkergen-bit <sandugash.zhetkergen@narxoz.kz>
2026-04-10 11:40:21 +0500

    Add files via upload

---
+++ 10apta.py
@@ -0,0 +1,48 @@
+import pandas as pd
+import matplotlib.pyplot as plt
+
+class ReportAnalyzer:
+    def __init__(self, path):
+        self.path = path
+        self.data = None
+
+    def load(self):
+        self.data = pd.read_csv(self.path)
+
+    def style(self, title, x, y):
+        plt.title(title)
+        plt.xlabel(x)
+        plt.ylabel(y)
+        plt.grid()
+
+    def graph1(self):
+        col = self.data.select_dtypes(include="number").columns[0]
+        plt.figure()
+        plt.plot(self.data[col])
+        self.style("Graph 1", "Index", col)
+        plt.show()
+
+    def graph2(self):
+        col = self.data.select_dtypes(include="number").columns[1]
+        plt.figure()
+        plt.plot(self.data[col])
+        self.style("Graph 2", "Index", col)
+        plt.show()
+
+    def graph3(self):
+        col = self.data.select_dtypes(include="number").columns[2]
+        plt.figure()
+        plt.plot(self.data[col])
+        self.style("Graph 3", "Index", col)
+        plt.show()
+
+
+if __name__ == "__main__":
+    print("Zhetkergen Sandugash")
+
+    ra = ReportAnalyzer("report.csv")
+    ra.load()
+
+    ra.graph1()
+    ra.graph2()
+    ra.graph3()
